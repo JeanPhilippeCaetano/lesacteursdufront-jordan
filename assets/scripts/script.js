5dcabe2e3492e4720d2d18b49e9e8995b36f12e3
JCAETANO <caetano.jphilippe@gmail.com>
2022-11-16 13:00:31 +0100

    quasi finished

---
+++ assets/scripts/script.js
@@ -0,0 +1,45 @@
+let images = [
+    "../assets/images/logos/sneaker.png",
+    "../assets/images/logos/sneaker2.jfif",
+    "../assets/images/logos/sneaker3.png",
+    "../assets/images/logos/sneaker4.png"
+]
+
+let currentPage = 0
+
+const product = document.querySelector(".container-products .container-products-content")
+
+const generateDiv = (url) => {
+    const div = `<img src=${url} style="width:100%; justify-content:center;height:100%;align-items:center";margin:auto>    `
+    product.innerHTML += div
+}
+
+const init = () => {
+    product.innerHTML = ""
+    generateDiv(images[currentPage])
+}
+
+const next = () => {
+    product.innerHTML = ""
+    if (currentPage + 1 < images.length) {
+        currentPage++
+    } else {
+        currentPage = 0
+    }
+    console.log(currentPage)
+    generateDiv(images[currentPage])
+}
+
+const previous = () => {
+    product.innerHTML = ""
+    if (currentPage - 1 < 0) {
+        currentPage = images.length - 1
+    } else {
+        currentPage--
+    }
+    console.log(currentPage)
+    generateDiv(images[currentPage])
+}
+
+
+init()
